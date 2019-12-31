eaca4e5544b108b8cd8efd930ae089de506d9377
benthebear93 <nswve@naver.com>
2019-12-31 09:53:49 +0900

    3days

---
+++ bakjoon1697.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+int line[100000];
+int visited[100000] = {0,};
+int n, k;
+int time;
+
+int bfs() {
+	time = 0;
+	queue<int> q; 
+	q.push(n);
+	while (!q.empty()) {
+		
+		int size = q.size();
+ 		for (int i = 0; i < size; i++) {
+			n = q.front();
+			q.pop();
+			if (n == k){
+				return time;
+			}
+			if (n > 0 && visited[n - 1] == 0) {
+				q.push(n - 1);
+				visited[n - 1] = 1;
+			}
+			if (n < 100000 && visited[n + 1] == 0) {
+				q.push(n + 1);
+				visited[n + 1] = 1;
+			}
+			if (n * 2 < 100000 && visited[n * 2] == 0) {
+				q.push(n * 2);
+				visited[n * 2] = 1;
+			}
+		}
+		time++;
+	}
+}
+
+int main() {
+	cin >> n >> k;
+	time = bfs();
+	cout << time << endl;
+}
