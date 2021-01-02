657e79a3d81727d2e4ecbcb1f3e7ca17f4372ac2
benthebear93 <nswve@naver.com>
2021-01-02 23:59:46 +0900

    day18

--- Baekjoon1175.py
+++ Baekjoon1175.py
@@ -2,15 +2,27 @@ import numpy as np
 from collections import deque
 M, N = map(int, input().split())  # map input #n row, m column
 map = []
+visited = []
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 temp_x = 0
 temp_y = 0
+st_pos = [0 , 0]
+c_pos = [0, 0]
 if N > 50 and M > 50:
     print("M , N has to be smaller & equal then 50")
 else:
     for a in (0, N):
         map.append(list(input()))
+
+for i in (0, N):
+    for j in (0, M):
+        if map[i][j] == 'S':
+            st_pos[0] = i
+            st_pos[1] = j
+        elif map[i][j] == 'C':
+            c_pos[0] = i
+            c_pos[1] = j
 
 def bfs():
     queue = deque()
